08a1a237d89b3cee91eea608ebd8a6dd97e37b25
Kabir_Sagar_Sagi <62954955+Kabir-Sagi@users.noreply.github.com>
2021-03-10 09:51:20 +0530

    Add files via upload

---
+++ JSON.js
@@ -0,0 +1,19 @@
+var data = {
+   
+}
+
+// console.log(data)
+// console.log(data.name)
+// console.log(data.city) 
+
+// console.log(data["name"])
+// console.log(data["name 1"])
+// console.log(data)
+ data.state = "Harayana"
+ console.log(data)
+
+
+ 
+
+  data.city = "delhi"
+  console.log(data)
---
+++ function.js
@@ -0,0 +1,10 @@
+function addition(o,p) {
+    var a = o;
+    var b = p;
+    var c = a +b 
+    console.log(c)
+}
+  addition(5,9)
+  addition(100,100);
+  addition("sagar"," sagi")
+ 
